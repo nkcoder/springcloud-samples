dc6b609b9a0538f024954e0cabebccc00d552550
daniel <daniel5hbs@gmail.com>
2020-04-10 22:39:04 +0800

    initial commit

---
+++ microservice-one/src/main/java/org/nkcoder/cloud/ServiceOneApplication.java
@@ -0,0 +1,13 @@
+package org.nkcoder.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceOneApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ServiceOneApplication.class, args);
+  }
+
+}
